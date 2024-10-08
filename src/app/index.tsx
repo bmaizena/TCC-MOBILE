b2c1a08915066bf14534872e66a5f90b6b4d2f17
Daniel Sallesse <116963571+bmaizena@users.noreply.github.com>
2024-10-08 11:17:13 -0300

    up

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,54 +1,61 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { useEffect, useState } from "react";
+import { Dimensions, Text, View } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Link } from "expo-router";
 
+export default function Mapa2(){
+    const windowWidth = Dimensions.get("window").width;
+    const windowHeight = Dimensions.get("window").height;
+      
+    const [currentLocation, setCurrentLocation] = useState(null);
+    const [initialRegion, setInitialRegion] = useState(null);
+      
+    useEffect(() => {
+        const getLocation = async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                console.log("Permissão de acesso à localização negada!");
+                return;
+            }
+        
+            let location = await Location.getCurrentPositionAsync({});
+            setCurrentLocation(location.coords);
+        
+            setInitialRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            });
+        };
+      
+        getLocation();
+    }, []);
+      
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>MapView - 2</Text>
+            <Link href={'./login'}>Login</Link>
 
-export default function LoginScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = () => {
-    if (email === '' || password === '') {
-      Alert.alert('Erro', 'Por favor, preencha todos os campos.');
-    } else {
-      Alert.alert('Sucesso', `Email: ${email}\nSenha: ${password}`);
-    }
-  };
-
-  return (
-    
-      <View className="flex-1 justify-center items-center bg-gray-100 p-6">
-        <Text className="text-4xl font-bold text-blue-600 mb-10">Login</Text>
-
-        {/* Campo de Email */}
-        <TextInput
-          className="border border-gray-300 rounded-lg w-full p-4 mb-4 bg-white"
-          placeholder="Digite seu e-mail"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-
-        {/* Campo de Senha */}
-        <TextInput
-          className="border border-gray-300 rounded-lg w-full p-4 mb-6 bg-white"
-          placeholder="Digite sua senha"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry={true}
-          autoCapitalize="none"
-        />
-
-        {/* Botão de Login */}
-        <TouchableOpacity className="bg-blue-500 p-4 rounded-lg w-full" onPress={handleLogin}>
-          <Text className="text-white text-center text-lg">Entrar</Text>
-        </TouchableOpacity>
-
-        {/* Esqueceu a Senha */}
-        <TouchableOpacity className="mt-4" onPress={() => Alert.alert('Esqueceu a senha', 'Link para recuperar senha')}>
-          <Text className="text-blue-500">Esqueceu sua senha?</Text>
-        </TouchableOpacity>
-      </View>
-    
-  );
+            {initialRegion && (
+                <MapView 
+                    style={{width:'100%', height:'90%'}} 
+                    initialRegion={initialRegion} 
+                    showsUserLocation
+                >
+                
+                    {currentLocation && (
+                        <Marker
+                            coordinate={{
+                                latitude: currentLocation.latitude,
+                                longitude: currentLocation.longitude,
+                            }}
+                        title="Your Location"
+                        />
+                    )}
+                </MapView>
+            )}
+        </View>
+    );
 }
